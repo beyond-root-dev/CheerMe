75ad0502c9cd5becab7db9d4dc52658e0f2d446b
Neeraj Negi <neerajnegi@beyondroot.com>
2019-11-26 13:15:42 +0530

    Initial Commit

---
+++ CheerMe/Sources/CheerMe.h
@@ -0,0 +1,19 @@
+//
+//  CheerMe.h
+//  CheerMe
+//
+//  Created by Admin on 11/25/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheerMe.
+FOUNDATION_EXPORT double CheerMeVersionNumber;
+
+//! Project version string for CheerMe.
+FOUNDATION_EXPORT const unsigned char CheerMeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheerMe/PublicHeader.h>
+
+
