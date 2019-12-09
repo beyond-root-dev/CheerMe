680da5ca845bc816c5d2c759046cb3d123f6d2d0
Neeraj Negi <neerajnegi@beyondroot.com>
2019-12-09 10:35:42 +0530

    Present View

--- CheerMe/Sources/CMFloatButton.swift
+++ CheerMe/Sources/CMFloatButton.swift
@@ -2,40 +2,21 @@
 //  CMFloatButton.swift
 //  CheerMe
 //
-//  Created by Admin on 11/25/19.
+//  Created by Admin on 12/6/19.
 //  Copyright © 2019 Admin. All rights reserved.
 //
 
 import UIKit
 
-@IBDesignable public class CMFloatButton: UIButton {
+class CMFloatButton: UIButton {
 
-    let nibName = "CMFloatButton"
-    var contentView: UIView!
-    
-    // MARK:- Set Up View
-    public override init(frame: CGRect) {
-     // For use in code
-      super.init(frame: frame)
-      setUpView()
+    @IBOutlet weak var buttonImage: UIImageView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
     }
-
-    public required init?(coder aDecoder: NSCoder) {
-       // For use in Interface Builder
-       super.init(coder: aDecoder)
-      setUpView()
-    }
-    
-    private func setUpView() {
-        let bundle = Bundle(for: type(of: self))
-        let nib = UINib(nibName: self.nibName, bundle: bundle)
-        self.contentView = nib.instantiate(withOwner: self, options: nil).first as? UIView
-        addSubview(contentView)
-        
-//        contentView.center = self.center
-//        contentView.autoresizingMask = []
-//        contentView.translatesAutoresizingMaskIntoConstraints = true
-      
-    }
+    */
 
 }
